9dfff482260c4a2eb751e5d8a163d0fe4cf9a287
leicam <julianoleicam@gmail.com>
2025-05-07 16:21:43 -0300

    feat: cors config

---
+++ umfgcloud.autenticacao.webapi/Extensions/CorsExtensions.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.Extensions.Options;
+
+namespace umfgcloud.autenticacao.webapi.Extensions;
+
+internal static class CorsExtensions
+{
+    /// <summary>
+    /// Adicionar CORS com uma política personalizada
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
+    internal static void AddCorsPolicy(this IServiceCollection services, IConfiguration configuration)
+    {
+        var name = (configuration.GetSection(nameof(CorsOptions)))[nameof(CorsOptions.DefaultPolicyName)]?.Trim() ?? string.Empty;
+
+        services.Configure<CorsOptions>(options => { options.DefaultPolicyName = name; });
+        services.AllowAny(name);
+    }
+
+    /// <summary>
+    /// Configura o pipeline de requisição, utilizando a política definida no arquivo de configuracao
+    /// </summary>
+    /// <param name="app"></param>
+    internal static void UseCorsPolicy(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var options = scope.ServiceProvider.GetRequiredService<IOptionsSnapshot<CorsOptions>>().Value;
+
+        app.UseAny(options.DefaultPolicyName);
+    }
+
+    /// <summary>
+    /// Permite qualquer origem (Cuidado com segurança, pode restringir conforme necessário)
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="name"></param>
+    private static void AllowAny(this IServiceCollection services, string name)
+    {
+        if (name != "AllowAllOrigins")
+            return;
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAllOrigins", policy =>
+            {
+                policy.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+        });
+    }
+
+    private static void UseAny(this IApplicationBuilder app, string name)
+    {
+        if (name != "AllowAllOrigins")
+            return;
+
+        app.UseCors("AllowAllOrigins");
+    }
+}
--- umfgcloud.autenticacao.webapi/Program.cs
+++ umfgcloud.autenticacao.webapi/Program.cs
@@ -17,11 +17,13 @@ namespace umfgcloud.autenticacao.webapi
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwagger();
+            builder.Services.AddCorsPolicy(builder.Configuration);
             builder.Services.AddAutenticacao(builder.Configuration);
             builder.Services.RegistrarServicos(builder.Configuration);
 
             var app = builder.Build();
 
+            app.UseCorsPolicy();
             app.UseSwaggerUI();
             app.UseHttpsRedirection();
             app.UseAuthorization();
